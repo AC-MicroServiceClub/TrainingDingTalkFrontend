a6b06904330a9123bdb3b71e4ac5e59be5f0a8e4
86136 <86136@LAPTOP-IMFAO393>
2018-10-14 00:23:55 +0800

    add

--- src/services/user.js
+++ src/services/user.js
@@ -21,12 +21,6 @@ if ('notInDingTalk' != dd.env.platform) {
           );
           // 通过免登录code获取userid并保存到常量userid对象中
           userid.val = restful.get('https://oapi.dingtalk.com/user/getuserinfo', {access_token: '', code: result.code});
-          ReactDOM.render(
-            <NoticeBar mode="link" action={<span>Debug</span>}>
-              {JSON.stringify(userid)}
-            </NoticeBar>,
-            document.getElementById("notice")
-          );
         },
         onFail : function(err) {}
     });
