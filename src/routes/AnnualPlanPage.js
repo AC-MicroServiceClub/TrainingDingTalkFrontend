667f207d8e89711e509c6a28b493a31ad3a9a645
86136 <86136@LAPTOP-IMFAO393>
2018-10-13 22:41:49 +0800

    add

--- src/routes/AnnualPlanPage.js
+++ src/routes/AnnualPlanPage.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { connect } from 'dva';
-import { NavBar, Icon } from 'antd-mobile';
+import { NavBar, Icon, Tabs, Badge, List, Item } from 'antd-mobile';
 import styles from './AnnualPlanPage.css';
+
+const tabs = [
+  { title: <span>1月</span> },
+  { title: <span>2月</span> },
+  { title: <span>3月</span> },
+  { title: <span>4月</span> },
+  { title: <span>5月</span> },
+  { title: <span>6月</span> },
+  { title: <span>7月</span> },
+  { title: <span>8月</span> },
+  { title: <span>9月</span> },
+  { title: <span>10月</span> },
+  { title: <span>11月</span> },
+  { title: <span>12月</span> }
+];
 
 @connect(({ user, menus }) => ({
   user: user,
@@ -16,10 +31,25 @@ export default class AnnualPlanPage extends React.Component {
 
   render() {
     return (
-      <NavBar
-        mode="light"
-        icon={<Icon type="left" />}
-        onLeftClick={() => this.props.history.push('/')}>{this.props.menus.annualPlan.text}</NavBar>
+      <div>
+        <NavBar
+          mode="light"
+          icon={<Icon type="left" />}
+          onLeftClick={() => this.props.history.push('/')}>{this.props.menus.annualPlan.text}</NavBar>
+        <Tabs tabs={tabs}
+          initialPage={1}
+          onChange={(tab, index) => { console.log('onChange', index, tab); }}
+          onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}>
+        </Tabs>
+        <List renderHeader={() => 'Basic Style'}>
+          <List.Item extra={'extra content'}>Title</List.Item>
+          <List.Item extra={'extra content'}>Title</List.Item>
+          <List.Item extra={'extra content'}>Title</List.Item>
+          <List.Item extra={'extra content'}>Title</List.Item>
+          <List.Item extra={'extra content'}>Title</List.Item>
+          <List.Item extra={'extra content'}>Title</List.Item>
+        </List>
+      </div>
     );
   }
 }
