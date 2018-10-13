70bcf5051d1b5f9c49cdb71dc8b7f479dea9e151
86136 <86136@LAPTOP-IMFAO393>
2018-10-13 20:15:25 +0800

    增加判断dd.env.platform

--- src/services/user.js
+++ src/services/user.js
@@ -6,16 +6,19 @@ const userid = {
 };
 
 // 免登录设置
-dd.ready(function() {
-  dd.runtime.permission.requestAuthCode({
-    corpId: 'ding11e2d6305c40beeb35c2f4657eb6378f',
-      onSuccess: function(result) {
-        // 通过免登录code获取userid并保存到常量userid对象中
-        userid.val = restful.get('https://oapi.dingtalk.com/user/getuserinfo', {access_token: '', code: result.code});
-      },
-      onFail : function(err) {}
+if ('notInDingTalk' != dd.env.platform) {
+  dd.ready(function() {
+    dd.env.platform = 'dingTalk';
+    dd.runtime.permission.requestAuthCode({
+      corpId: 'ding11e2d6305c40beeb35c2f4657eb6378f',
+        onSuccess: function(result) {
+          // 通过免登录code获取userid并保存到常量userid对象中
+          userid.val = restful.get('https://oapi.dingtalk.com/user/getuserinfo', {access_token: '', code: result.code});
+        },
+        onFail : function(err) {}
     });
-});
+  });
+}
 
 export function getUserInfo() {
   // 从userid中获取当前登录用户的userid
