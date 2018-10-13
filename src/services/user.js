a0a7aa109a6aaabb69b249a234fedf23c83aba1c
86136 <86136@LAPTOP-IMFAO393>
2018-10-13 23:51:44 +0800

    add

--- src/services/user.js
+++ src/services/user.js
@@ -1,18 +1,30 @@
 import restful from '../utils/restful';
 import * as dd from 'dingtalk-jsapi';
+import ReactDOM from 'react-dom';
+import { NoticeBar, Icon } from 'antd-mobile';
 
 const userid = {
 	val: ''
 };
 
 // 免登录设置
-console.info(JSON.stringify(dd));
+ReactDOM.render(
+  <NoticeBar mode="link" action={<span>Debug</span>}>
+    {JSON.stringify(dd)}
+  </NoticeBar>,
+  document.getElementById("notice")
+);
 if ('notInDingTalk' != dd.env.platform) {
   dd.ready(function() {
     dd.runtime.permission.requestAuthCode({
       corpId: 'ding11e2d6305c40beeb35c2f4657eb6378f',
         onSuccess: function(result) {
-          console.info(result);
+          ReactDOM.render(
+            <NoticeBar mode="link" action={<span>Debug</span>}>
+              {JSON.stringify(result)}
+            </NoticeBar>,
+            document.getElementById("notice")
+          );
           // 通过免登录code获取userid并保存到常量userid对象中
           userid.val = restful.get('https://oapi.dingtalk.com/user/getuserinfo', {access_token: '', code: result.code});
         },
